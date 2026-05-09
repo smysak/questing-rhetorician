9da4f68541b0d969cff29df103995cb3d43ca105
smysak <98991422+smysak@users.noreply.github.com>
2026-05-09 14:56:41 +0800

    shuffle the choices that the player will be asked

--- gamescript.js
+++ gamescript.js
@@ -32,6 +32,16 @@ const dictionChoices = [
 	}
 ]
 
+function shuffle(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}
+
+shuffle(dictionChoices);
+
 const gamespace = document.getElementById('gamespaceArea');
 const buttons = document.querySelectorAll('button');
 const intro = document.getElementById('introduction');
